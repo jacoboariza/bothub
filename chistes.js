8bbbc20325dab63273e915f41481a874253daddd
Jacobo Ariza <luis.jacobo@gmail.com>
2017-12-27 02:17:50 +0000

    chistes

--- chistes.js
+++ chistes.js
@@ -1,24 +1,27 @@
 var sqlite3 = require('sqlite3').verbose();
 var util = require('util');
-var db = new sqlite3.Database('./bot.db');
-var usersDB = new sqlite3.Database("./bot.db");
-var id;
+
 
 function aprender_chiste(texto, callback){
 
-  usersDB.serialize(function() {
-
+  //usersDB.serialize(function() {
+var db = new sqlite3.Database('./bot.db');
+var usersDB = new sqlite3.Database("./bot.db");
+var id;
   usersDB.each("SELECT MAX(ID) AS maxid FROM CHISTES; ", function(err, row) {
       id= row.maxid + 1;
       db.run("INSERT into CHISTES(ID,CONTENIDO,CONTADOR) VALUES ('"+id+"','"+texto+"','0')");
+      usersDB.close();
+      db.close();
+      callback();
   });
 
-  usersDB.each("SELECT CONTENIDO AS contenido FROM CHISTES", function(err, row) {
-      console.log(row.contenido + ": ");
-  });
-});
+  //usersDB.each("SELECT CONTENIDO AS contenido FROM CHISTES", function(err, row) {
+  //    console.log(row.contenido + ": ");
+  //});
+//});
 
-usersDB.close();
+
 }
 
 
