2977356fb71b66c1cd60b7f780530d908aded244
Jacobo Ariza <luis.jacobo@gmail.com>
2017-12-30 01:47:50 +0000

    update

--- index.js
+++ index.js
@@ -11,19 +11,6 @@ const regalo  = require("./regalo");
 
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-
-
 
 
 //const chistes = require("./chistes");
